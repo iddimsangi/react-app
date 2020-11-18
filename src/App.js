76a9abf998376720620b718db09e8d2293905595
iddimsangi <adammsangi@gmail.com>
2020-11-18 12:19:40 +0300

    add state and dynamic data change

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+
+
 class App extends Component {
+  state = {
+    person:[
+      {name:'iddimsangi'},
+      {name:'harith'}
+    ],
+    otherState:'im the other state'
+  }
+   
+  
+  switchHandler = () =>{
+    // console.log('you clicked');
+    this.setState({
+      person:[
+        {name:'iddi Adam'},
+        {name:'harith iddi'}
+      ]
+    })
+  }
   render() {
     return (
       <div className="App">
-       <h1>Hi i'm new react developer</h1>
-      <Person />
+       <h1>Welcome to react software devs</h1>
+       <button onClick={this.switchHandler}>switch</button>
+      <Person name={this.state.person[0].name} />
+      <Person name="harith" />
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
-const detail = () =>{
-    return <h2>imported first component</h2>
+const detail = (props) =>{
+    return <h2>Hi i'm {props.name}  new react developer</h2>;
 }
 export default detail;
